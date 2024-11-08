b5cc4a50da02b0c88d6161c9fdb2b505d78da036
Farzana Akter Nipa <nipa20farzana@gmail.com>
2024-11-09 00:57:49 +0600

    Created Projects model

--- project_manager/accounts/models.py
+++ project_manager/accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 # Create your models here.
 
@@ -16,4 +17,14 @@ class User(AbstractUser):
     def __str__(self):
         return self.username
     
+
+class Projects(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='owneed_projects')
+    created_at = models.DateTimeField(auto_now_add=True)
     
+    def __str__(self):
+        return self.name
+
+                            
