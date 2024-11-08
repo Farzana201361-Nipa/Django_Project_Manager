1063816f5ec6bf9c89c9de3d77389b3393db7c0f
Farzana Akter Nipa <nipa20farzana@gmail.com>
2024-11-09 00:25:17 +0600

    Created an app "acccounts' for handling all funtionalities and models

---
+++ project_manager/accounts/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class User(AbstractUser):
+    #id = models.AutoField(primary_key=True)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+    
+    def __str__(self):
+        return self.username
+    
+    
