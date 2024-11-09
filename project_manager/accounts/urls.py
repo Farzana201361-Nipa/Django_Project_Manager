66a49bde6fb727c5d24e16b7c6d840b1b9619020
Farzana Akter Nipa <nipa20farzana@gmail.com>
2024-11-10 01:37:32 +0600

    Created router and registered the viewsets

--- project_manager/accounts/urls.py
+++ project_manager/accounts/urls.py
@@ -1,9 +1,19 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
 
 app_name = 'accounts'
 
+#Creating router
+router = DefaultRouter()
+#registering each viewsets with a route
+router.register(r'users', views.UserViewSet)
+router.register(r'projects', views.ProjectsViewSet)
+router.register(r'project-members', views.ProjectMembersViewSet)
+router.register(r'tasks', views.TasksViewSet)
+router.register(r'comments', views.CommentsViewSet)
+
 urlpatterns = [
-    path('',views.home, name = 'home'),
+    path('', include(router.urls)),
     
 ]
