ac65a9784b5d17c3ae4234fad0d8d0ecbdde4823
Farzana Akter Nipa <nipa20farzana@gmail.com>
2024-11-10 00:26:40 +0600

    Updated realted serializers

--- project_manager/accounts/serializers.py
+++ project_manager/accounts/serializers.py
@@ -10,23 +10,31 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'first_name', 'last_name', 'date_joined']
     
 class ProjectsSerializer(serializers.ModelSerializer):
+    owner = UserSerializer(read_only = True)
     class Meta:
         model = Projects
         fields = ['id', 'name', 'description', 'owner', 'created_at']
         
         
 class ProjectMemberSerializer(serializers.ModelSerializer):
+    project = ProjectsSerializer(read_only = True)
+    user = UserSerializer(read_only = True)
     class Meta:
         model = ProjectMembers
         fields = ['id', 'project', 'user', 'role']
         
 class TasksSerializer(serializers.ModelSerializer):
+    assigned_to = UserSerializer(read_only = True)
+    project = ProjectsSerializer(read_only = True)
+    
     class Meta:
         model = Tasks
         fields = ['id', 'title', 'description', 'status','priority', 'assigned_to', 'project','created_at', 'due_date' ]
         
         
 class CommentsSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only = True)
+    task =  TasksSerializer(read_only = True)
     class Meta:
         model = Comments
         fields = ['id', 'content', 'user', 'task', 'created_at']
