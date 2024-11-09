64d4ec868a017eedc855c49dca796b3fe00650b8
Farzana Akter Nipa <nipa20farzana@gmail.com>
2024-11-09 23:13:16 +0600

    Admin set up for all models

--- project_manager/accounts/admin.py
+++ project_manager/accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . models import Projects, ProjectMembers, Tasks, Comments
+
 
 # Register your models here.
+admin.site.register(Projects)
+admin.site.register(ProjectMembers)
+admin.site.register(Tasks)
+admin.site.register(Comments)
+
