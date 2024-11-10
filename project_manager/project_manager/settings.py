7df2fccc13d310b5a114a3b9bfdefed2a707707e
Farzana Akter Nipa <nipa20farzana@gmail.com>
2024-11-10 15:09:31 +0600

    To prevent the expiration of Tokens

--- project_manager/project_manager/settings.py
+++ project_manager/project_manager/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,6 +120,11 @@ REST_FRAMEWORK ={
     
 }
 
+#To increase the lifespan of access token
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=50),  
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),     
+}
 
 
 # Internationalization
