79bb7a339e908fa61f71ea7d7c3107c3686e2576
Farzana Akter Nipa <nipa20farzana@gmail.com>
2024-11-10 12:18:52 +0600

    Both access and refresh token are working

--- project_manager/project_manager/urls.py
+++ project_manager/project_manager/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('accounts.urls', namespace='accounts')),
     path('api/token/', TokenObtainPairView.as_view(), name= 'token_obtain_pair'),
-    path('api/token/refreesh', TokenRefreshView.as_view(), name= 'token_refresh'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name= 'token_refresh'),
     
 ]
